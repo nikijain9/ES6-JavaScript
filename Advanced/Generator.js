385a2c487e6213368be025c30e42019477e958ce
Nikita <100424127+nikijain9@users.noreply.github.com>
2022-11-06 12:35:13 +0530

    Generators in JS

---
+++ Advanced/Generator.js
@@ -0,0 +1,24 @@
+//The Generator object is returned by a generator function and it conforms to both the iterable protocol and the iterator protocol.
+
+/*
+Using a generator,
+
+you can stop the execution of a function from anywhere inside the function using the 'yield' keyword
+and continue executing code from a halted position
+*/
+
+function* generator_function() {
+    yield "Hello";
+    yield "World";
+}
+
+const generator_object = generator_function();
+
+console.log(generator_object.next());
+console.log(generator_object.next());
+console.log(generator_object.next());
+
+
+for (const word of generator_object) {
+    console.log(word);
+}
