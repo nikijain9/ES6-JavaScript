ec11229198a2426b0cf93f7201718920381190cf
Nikita <100424127+nikijain9@users.noreply.github.com>
2022-12-01 20:28:44 +0530

    Added gitignore and Operators and Expressions in JS

---
+++ CWH_Fundamentals/Op&Exp.js
@@ -0,0 +1,44 @@
+console. log("Operators in Js")
+// Arithmetic Operators
+let a = 10;
+let b= 4;
+
+console. log("a + b =", a + b)      //14
+console. log("a - b =", a - b)      //6
+console. log("a / b =", a / b)      //2.5
+console. log("a ** b = ", a ** b)   //10*10*10*10=10000
+console. log("a % b = ", a % b)     //2
+console. log("++a = ", ++a )        //11     
+console. log("a++ = ", a++ )        //11
+console. log("--a = ", --a )        //11
+console. log("a-- = ", a-- )        //11
+console. log("a = ", a )            //10
+console. log("a-- = ", a-- )        //10
+console. log("a = ", a )            //9
+
+
+//Assignment Operators
+let assignment = 5;
+assignment += 6;
+console.log(assignment);
+
+//Comparison Operators
+let comp1 = 6;
+let comp2 = "6";
+// let comp3 = "6";
+
+console. log("comp1 == comp2 is ", comp1 == comp2)
+console. log("compi != comp2 is ", comp1 != comp2)
+console. log("compi === comp2 is ", comp1 === comp2)
+console. log("compi !== comp2 is ", comp1 !== comp2)
+
+//Logical Operators
+let x = 5;
+let y = 6;
+console.log(x < y && x != 5);       //false
+console.log(x < y && x == 5);       //true
+console.log(!false);            
+console.log(!true);
+
+
+
