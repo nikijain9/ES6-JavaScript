a566dab000a935f6fcae006f783acb2e2f6bffe3
Nikita <100424127+nikijain9@users.noreply.github.com>
2022-12-03 01:15:39 +0530

    Strings and String literals in JS

---
+++ CWH_Fundamentals/strings.js
@@ -0,0 +1,20 @@
+//Strings are used to store and manipulate text
+
+let naming = "Harry"
+// console.log(name.length)
+console.log(naming[0])
+console.log(naming[1])
+let friend = 'Prakash'
+console.log(friend)
+// let myfriend = "Shubh' // Dont do this
+
+// Template literals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+let fruit = 'Banana\'s'
+console.log(fruit)
